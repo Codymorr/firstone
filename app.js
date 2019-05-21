a1d9d0f4c07a3bdd5720a64bca07e2970ba8d851
Codymorr <codymorr.developer@gmail.com>
2019-05-21 17:30:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -26,15 +26,17 @@ app.use(express.static(__dirname + '/app/public'));
 
 // build mongo database connection url //
 
-process.env.DB_HOST = process.env.DB_HOST || 'mongo'
-process.env.DB_PORT = process.env.DB_PORT || 27017;
-process.env.DB_NAME = process.env.DB_NAME || 'node-login';
+process.env.DB_HOST = process.env.DB_HOST || 'mongo';
+process.env.DB_USER = process.env.DB_USER || 'mongo';
+process.env.DB_PORT = process.env.DB_PORT || 'ds259586.mlab.com:59586';
+process.env.DB_NAME = process.env.DB_NAME || 'heroku_t8xqw6t1';
+process.env.DB_PASS = process.env.DB_PASS || '4M3Gr_FGyfjks!v'
 
 if (app.get('env') != 'live'){
-	process.env.DB_URL = 'mongodb://mongo:4M3Gr_FGyfjks!v@ds259586.mlab.com:59586/heroku_t8xqw6t1';
+	process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT;
 }	else {
 // prepend url with authentication credentials // 
-	process.env.DB_URL = 'mongodb://mongo:4M3Gr_FGyfjks!v@ds259586.mlab.com:59586/heroku_t8xqw6t1';
+	process.env.DB_URL = 'mongodb://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST+':'+process.env.DB_PORT;
 }
 
 app.use(session({
