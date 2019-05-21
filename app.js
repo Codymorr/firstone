bdb8efdd4254623203aaa35f0cfc6421e45352dc
Codymorr <codymorr.developer@gmail.com>
2019-05-21 17:31:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ process.env.DB_HOST = process.env.DB_HOST || 'mongo';
 process.env.DB_USER = process.env.DB_USER || 'mongo';
 process.env.DB_PORT = process.env.DB_PORT || 'ds259586.mlab.com:59586';
 process.env.DB_NAME = process.env.DB_NAME || 'heroku_t8xqw6t1';
-process.env.DB_PASS = process.env.DB_PASS || '4M3Gr_FGyfjks!v'
+process.env.DB_PASS = process.env.DB_PASS || '4M3Gr_FGyfjks!v';
 
 if (app.get('env') != 'live'){
 	process.env.DB_URL = 'mongodb://'+process.env.DB_HOST+':'+process.env.DB_PORT;
